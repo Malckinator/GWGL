3b9085db0b95c241fd95a3bec8cd64d8b093f74d
Malckinator <malckinator@gmail.com>
2022-09-06 20:10:31 +0100

    ebig trangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+#include <glfw3.h>
 
-int main() {
-	std::cout << "Hello World!\n";
+int main(void) {
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
